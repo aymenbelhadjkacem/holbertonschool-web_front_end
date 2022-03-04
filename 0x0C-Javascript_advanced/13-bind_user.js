04adc07a6a58a6d976c307a6571757a602c49594
aymenbelhadjkacem <aymenbelhadjkacem>
2022-03-04 10:38:20 -0500

    bind

---
+++ 0x0C-Javascript_advanced/13-bind_user.js
@@ -0,0 +1,15 @@
+var user = { hobby: 'Calligraphy', favoriteSport: 'Hockey', astrologicalSign: 'Aries', firstName: 'Buillaume', lastName: 'Ialva', location: 'Telaviv', occupation: 'Engineer' };
+
+function logWelcomeUser (welcomeString) {
+
+  return (`${welcomeString}, ${user.firstName}.Your occupation is: ${user.occupation}`);
+}
+
+
+
+var bindLogWelcomeUser = logWelcomeUser.bind(user);
+
+
+bindLogWelcomeUser('Welcome');
+
+console.log(bindLogWelcomeUser('Hello'));

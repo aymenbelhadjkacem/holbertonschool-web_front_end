d9f91959c978d0cc7f9d0e483859fdf94b6459c7
aymenbelhadjkacem <aymenbelhadjkacem>
2022-03-04 10:28:41 -0500

    task2

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,10 @@
+function welcomeMessage(fullName)
+{
+    alert('Welcome ' + fullName);
+}
+
+guillaume = welcomeMessage('Guillaume');
+
+alex = welcomeMessage('Alex');
+
+fred = welcomeMessage('Fred');
